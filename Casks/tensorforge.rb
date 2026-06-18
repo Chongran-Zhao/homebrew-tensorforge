4027d5edc95d4856107df25fbbd5e5bec6a80e2d
Chongran-Zhao <imchongran@gmail.com>
2026-06-18 16:46:55 +0800

    tensorforge 1.0.0

--- Casks/tensorforge.rb
+++ Casks/tensorforge.rb
@@ -1,6 +1,6 @@
 cask "tensorforge" do
   version "1.0.0"
-  sha256 "6939c0b95eb55ac960e60acef0190c8a90cccb0efced2ad5a08719c08679246a"
+  sha256 "312dcc3a6d27a3364dc4596b6e2b63c2bb6008ed960e549fe08edb1821bf7228"
 
   url "https://github.com/Chongran-Zhao/TensorForge/releases/download/v1.0.0/TensorForge-v#{version}-aarch64-apple-darwin.dmg"
   name "TensorForge"
